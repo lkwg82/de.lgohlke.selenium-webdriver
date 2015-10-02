d5b222128997dc38e9b59d63d24ff87218ba4953
Lars K.W. Gohlke <lkwg82@gmx.de>
2015-10-03 01:53:41 +0200

    fixed: LockingWebDriverInvocationHandler (missed locking for a single thread)

--- src/main/java/de/lgohlke/selenium/webdriver/concurrent/LockingWebDriverInvocationHandler.java
+++ src/main/java/de/lgohlke/selenium/webdriver/concurrent/LockingWebDriverInvocationHandler.java
@@ -7,44 +7,50 @@ import org.openqa.selenium.WebDriver;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.StampedLock;
 
 @RequiredArgsConstructor
 @Slf4j
 class LockingWebDriverInvocationHandler implements InvocationHandler {
-    private final StampedLock lock        = new StampedLock();
-    private final StampedLock runningLock = new StampedLock();
+    private final ReentrantLock threadLock  = new ReentrantLock();
+    private final StampedLock   runningLock = new StampedLock();
 
     private final WebDriver wrappedDriver;
-    private long stamp               = 0L;
-    private long threadIdHoldingLock = 0;
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        long runningStamp = runningLock.writeLock();
+
+        // only if no thread or this thread already holds this lock
+        threadLock.lock();
         try {
-            return synchronizedInvoke((LockingWebDriver) proxy, method, args);
+            // pass not more than one call at a time
+            long runningStamp = runningLock.writeLock();
+            try {
+                return synchronizedInvoke((LockingWebDriver) proxy, method, args);
+            } finally {
+                runningLock.unlockWrite(runningStamp);
+            }
         } finally {
-            runningLock.unlockWrite(runningStamp);
+            threadLock.unlock();
         }
     }
 
     private Object synchronizedInvoke(LockingWebDriver proxy, Method method, Object[] args) throws IllegalAccessException, InvocationTargetException {
         if (null == args && "lock".equals(method.getName())) {
-            stamp = lock.writeLock();
-            threadIdHoldingLock = Thread.currentThread().getId();
-            log.info("locking with {}", stamp);
+            threadLock.lock();
+            log.info("locking thread {}", Thread.currentThread().getId());
             return (UnlockLockingWebdriver) proxy::unlock;
         } else if (null == args && "unlock".equals(method.getName())) {
-            log.info("unlocking with {}", stamp);
-            lock.unlockWrite(stamp);
-            threadIdHoldingLock = 0;
+            log.info("release lock of thread {} with holdcount {}", Thread.currentThread().getId(), threadLock.getHoldCount());
+            threadLock.unlock();
             return null;
         } else if (null == args && "isLocked".equals(method.getName())) {
-            boolean writeLocked = lock.isWriteLocked();
-            log.info("is locked {}", writeLocked);
-            return writeLocked;
+            boolean locked = threadLock.isLocked();
+            log.info("is locked {}", locked);
+            return locked && threadLock.getHoldCount() > 1;
         } else {
+            log.info("calling {}", method.getName());
             return method.invoke(wrappedDriver, args);
         }
     }
--- src/test/java/de/lgohlke/selenium/webdriver/concurrent/LockingWebDriverInvocationHandlerTest.java
+++ src/test/java/de/lgohlke/selenium/webdriver/concurrent/LockingWebDriverInvocationHandlerTest.java
@@ -1,39 +1,34 @@
 package de.lgohlke.selenium.webdriver.concurrent;
 
+import com.google.common.collect.Lists;
 import de.lgohlke.selenium.webdriver.AbstractWebDriver;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.lang.reflect.Method;
+import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.StrictAssertions.assertThat;
 
 @Slf4j
 public class LockingWebDriverInvocationHandlerTest {
-
-    private static final int              TEST_TIMEOUT     = 3000;
-    private final        LockingWebDriver lockingWebdriver = ConcurrentWebDriver.createLocking(new AbstractWebDriver() {
-        @Override
-        public void close() {
-            super.close();
-        }
-    });
+    private static final int TEST_TIMEOUT = 3000;
 
     @Test(timeout = TEST_TIMEOUT)
-    public void shouldIndicateIfIsLocked() throws Throwable {
+    public void shouldIndicateIfIsLocked() {
 
         WebDriver driver = new AbstractWebDriver() {
             @Override
@@ -42,8 +37,7 @@ public class LockingWebDriverInvocationHandlerTest {
             }
         };
         LockingWebDriverInvocationHandler invocationHandler = new LockingWebDriverInvocationHandler(driver);
-
-        MethodCaller caller = new MethodCaller(invocationHandler);
+        MethodCaller                      caller            = new MethodCaller(invocationHandler);
 
         assertThat(caller.call("isLocked", Boolean.class)).isFalse();
         caller.call("lock");
@@ -52,164 +46,201 @@ public class LockingWebDriverInvocationHandlerTest {
         assertThat(caller.call("isLocked", Boolean.class)).isFalse();
     }
 
-    @Test(timeout = TEST_TIMEOUT)
-    @Ignore("wip")
+    @Test(timeout = TEST_TIMEOUT * 10)
     public void shouldHoldLockPerThread() throws InterruptedException {
         List<String> calledCommands = Collections.synchronizedList(new ArrayList<>());
 
         WebDriver driver = new AbstractWebDriver() {
             @Override
             public void get(String url) {
-                log.error("get");
-                calledCommands.add("get");
+                calledCommands.add("get " + url);
             }
 
             @Override
             public String getCurrentUrl() {
-                log.error("getCurrentUrl");
                 calledCommands.add("getCurrentUrl");
+                return "";
+            }
+
+            @Override
+            public String getPageSource() {
+                calledCommands.add("getPageSource");
                 return "";
             }
         };
 
-        LockingWebDriver lockingWebdriver = ConcurrentWebDriver.createLocking(driver);
+        LockingWebDriverInvocationHandler invocationHandler = new LockingWebDriverInvocationHandler(driver);
+        MethodCaller                      caller            = new MethodCaller(invocationHandler);
 
         Runnable longRunningThread = new Runnable() {
             @Override
             public void run() {
-                lockingWebdriver.lock();
-
-                sleep();
-                lockingWebdriver.get("a");
-                sleep();
-                lockingWebdriver.get("b");
-                sleep();
-
-                lockingWebdriver.unlock();
+                caller.call("lock");
+                // webdriver calls and sleeps in between
+                sleep(1);
+                caller.call("get", Void.class, "a");
+                sleep(1);
+                caller.call("get", Void.class, "b");
+                sleep(1);
+                caller.call("unlock");
             }
 
-            private void sleep() {
+            private void sleep(int microseconds) {
                 try {
-                    TimeUnit.MILLISECONDS.sleep(100);
+                    TimeUnit.MICROSECONDS.sleep(microseconds);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         };
 
-        Runnable timerThread = lockingWebdriver::getCurrentUrl;
+        Runnable timerThread  = () -> caller.call("getCurrentUrl", String.class);
+        Runnable timer2Thread = () -> caller.call("getPageSource");
 
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
-        executorService.submit(longRunningThread);
-        TimeUnit.MILLISECONDS.sleep(200);
-        executorService.submit(timerThread);
+        List<Runnable> actions    = Lists.newArrayList(longRunningThread, timerThread, timer2Thread);
+        List<Runnable> allACtions = new ArrayList<>();
+        IntStream.range(0, 1000).forEach(i -> actions.forEach(allACtions::add));
+
+        Collections.shuffle(allACtions, new SecureRandom());
+
+        ExecutorService executorService = Executors.newFixedThreadPool(200);
+
+        allACtions.parallelStream().forEach(executorService::submit);
 
         executorService.shutdown();
-        executorService.awaitTermination(10, TimeUnit.SECONDS);
+        executorService.awaitTermination(60, TimeUnit.SECONDS);
         executorService.shutdownNow();
 
-        Assertions.assertThat(calledCommands).containsExactly("get", "get", "getCurrentUrl");
+        int found = 0;
+        int size  = calledCommands.size();
+        for (int i = 0; i < size; i++) {
+            if ((i + 1) < size && calledCommands.get(i).equals("get a")) {
+                assertThat(calledCommands.get(i + 1)).describedAs(calledCommands + "").isEqualTo("get b");
+                found++;
+            }
+        }
+        assertThat(found).isEqualTo(1000);
     }
 
     @RequiredArgsConstructor
     private static class MethodCaller {
+        private static final LockingWebDriver PROXY = new LockingWebDriver() {
+            @Override
+            public UnlockLockingWebdriver lock() {
+                return () -> {
+                    // ok
+                };
+            }
+
+            @Override
+            public void unlock() {
+
+            }
+
+            @Override
+            public boolean isLocked() {
+                return false;
+            }
+
+            @Override
+            public void get(String url) {
+
+            }
+
+            @Override
+            public String getCurrentUrl() {
+                return null;
+            }
+
+            @Override
+            public String getTitle() {
+                return null;
+            }
+
+            @Override
+            public List<WebElement> findElements(By by) {
+                return null;
+            }
+
+            @Override
+            public WebElement findElement(By by) {
+                return null;
+            }
+
+            @Override
+            public String getPageSource() {
+                return null;
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public void quit() {
+
+            }
+
+            @Override
+            public Set<String> getWindowHandles() {
+                return null;
+            }
+
+            @Override
+            public String getWindowHandle() {
+                return null;
+            }
+
+            @Override
+            public TargetLocator switchTo() {
+                return null;
+            }
+
+            @Override
+            public Navigation navigate() {
+                return null;
+            }
+
+            @Override
+            public Options manage() {
+                return null;
+            }
+        };
         private final LockingWebDriverInvocationHandler invocationHandler;
 
-        public Object call(String name) throws Throwable {
+        public Object call(String name) {
             return call(name, Void.class);
         }
 
-        public <T> T call(String name, Class<T> castClass) throws Throwable {
+        @SuppressWarnings("unchecked")
+        public <T> T call(String name, Class<T> castClass, Object... args) {
 
-            Method method = LockingWebDriver.class.getDeclaredMethod(name);
-            Object invoke = invocationHandler.invoke(new LockingWebDriver() {
-                @Override
-                public UnlockLockingWebdriver lock() {
-                    return () -> {
-                        // ok
-                    };
+            List<Class> parameterTypes = new ArrayList<>();
+            Arrays.asList(args).forEach(a -> parameterTypes.add(a.getClass()));
+
+            try {
+                Method method;
+                try {
+                    method = LockingWebDriver.class.getDeclaredMethod(name);
+                } catch (NoSuchMethodException e) {
+                    if (parameterTypes.isEmpty()) {
+                        method = WebDriver.class.getDeclaredMethod(name);
+                    } else {
+                        method = WebDriver.class.getDeclaredMethod(name,
+                                                                   parameterTypes.toArray(new Class[parameterTypes.size()]));
+                    }
                 }
+                Object invoke = invocationHandler.invoke(PROXY, method, args.length == 0 ? null : args);
 
-                @Override
-                public void unlock() {
-
-                }
-
-                @Override
-                public boolean isLocked() {
-                    return false;
-                }
-
-                @Override
-                public void get(String url) {
-
-                }
-
-                @Override
-                public String getCurrentUrl() {
+                if (castClass.equals(Void.class)) {
                     return null;
                 }
-
-                @Override
-                public String getTitle() {
-                    return null;
-                }
-
-                @Override
-                public List<WebElement> findElements(By by) {
-                    return null;
-                }
-
-                @Override
-                public WebElement findElement(By by) {
-                    return null;
-                }
-
-                @Override
-                public String getPageSource() {
-                    return null;
-                }
-
-                @Override
-                public void close() {
-
-                }
-
-                @Override
-                public void quit() {
-
-                }
-
-                @Override
-                public Set<String> getWindowHandles() {
-                    return null;
-                }
-
-                @Override
-                public String getWindowHandle() {
-                    return null;
-                }
-
-                @Override
-                public TargetLocator switchTo() {
-                    return null;
-                }
-
-                @Override
-                public Navigation navigate() {
-                    return null;
-                }
-
-                @Override
-                public Options manage() {
-                    return null;
-                }
-            }, method, null);
-
-            if (castClass.equals(Void.class)) {
-                return null;
+                return (T) invoke;
+            } catch (Throwable e) {
+                log.error(e.getMessage(), e);
             }
-            return (T) invoke;
+            return null;
         }
     }
 
