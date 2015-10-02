ebbffcdeee1dadf165a19ee44cb28e4210528216
Lars K.W. Gohlke <lkwg82@gmx.de>
2015-10-02 14:06:31 +0200

    added xvfb startup script

---
+++ src/test/resources/xvfb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+#set +x
+
+display=$1
+cmd=$2
+
+case $cmd in
+ start)
+    Xvfb :$display.0 &
+    echo -n $! > xvfb.$display.pid
+    echo "started xvfb with display $display"
+ ;;
+ stop)
+    echo -n "kill xfvb on display $display ... "
+    kill $(cat xvfb.$display.pid) && echo ok && rm xvfb.$display.pid || echo failed
+ ;;
+ restart)
+    $0 $display start
+    $0 $display stop
+ ;;
+esac
