bce2f062c7eb871458dc05dfb93476a0747cf73c
Lars K.W. Gohlke <lkwg82@gmx.de>
2015-10-12 14:39:38 +0200

    added force push to add_to_latest_tag_changelog.sh to add changelog

--- add_to_latest_tag_changelog.sh
+++ add_to_latest_tag_changelog.sh
@@ -2,4 +2,7 @@
 
 git log --oneline $(git tag | tail -n2 |xargs | sed -e 's# #...#') | grep -v "\[maven-release-plugin\]" > target/changelog.txt
 
-git tag -a $(git tag | tail -n1) -f -F target/changelog.txt
+latestTag=$(git tag | tail -n1)
+git tag -a $latestTag -f -F target/changelog.txt
+
+git push --force origin refs/tags/$latestTag
