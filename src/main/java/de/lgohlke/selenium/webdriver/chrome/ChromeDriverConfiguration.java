8476938477ce715bf046a485708c1fbb57e75b66
Lars K.W. Gohlke <lkwg82@gmx.de>
2015-10-21 10:33:49 +0200

    added exposing chrome options to set custom

--- src/main/java/de/lgohlke/selenium/webdriver/chrome/ChromeDriverConfiguration.java
+++ src/main/java/de/lgohlke/selenium/webdriver/chrome/ChromeDriverConfiguration.java
@@ -1,13 +1,15 @@
 package de.lgohlke.selenium.webdriver.chrome;
 
 import de.lgohlke.selenium.webdriver.DriverConfiguration;
+import lombok.Getter;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class ChromeDriverConfiguration implements DriverConfiguration {
     private final DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-    private final ChromeOptions       options      = new ChromeOptions();
+    @Getter
+    private final ChromeOptions options = new ChromeOptions();
 
     @Override
     public Capabilities createCapabilities() {
