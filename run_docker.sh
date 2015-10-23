82542577f0e06bdc6c988cd74e875de74f4a9d36
Lars K.W. Gohlke <lkwg82@gmx.de>
2015-10-23 23:28:14 +0200

    cleanup script names

---
+++ run_docker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$MAVEN_REPODIR" ]; then
+    export MAVEN_REPODIR="-v /tmp/m2_docker:/root/.m2/repository";
+fi
+
+args="--name=webdriver-test-$$ -m 1G --memory-swap=-1 -v /tmp/webdriver-tests-$USER:/tmp/webdrivers-root -v /dev/shm/:/dev/shm -v /tmp/webdrivers_docker:/root/tmp_webdrivers $MAVEN_REPODIR "
+
+rm -rf docker_target
+docker build -t test-$$ . | tee docker_build.log
+export IMAGE_ID=$(tail -n1 docker_build.log| cut -d\  -f3)
+export CID=$(docker run -d $args $IMAGE_ID bash -c 'while(true);do sleep 1;done')
+
+echo -n $IMAGE_ID > docker_IMAGE_ID
+echo -n $CID > docker_CID
+
+function cleanup {
+  if [ -n "$JENKINS_HOME" ]; then
+     echo "don't cleanup leave it for jenkins"
+     exit 0
+  fi
+  run_docker_cleanup.sh
+}
+trap cleanup EXIT INT
+
+docker exec $CID ./run_tests.sh
+
+
--- run_tests.sh
+++ run_tests.sh
@@ -2,29 +2,9 @@
 
 set -e
 
-if [ -z "$MAVEN_REPODIR" ]; then
-    export MAVEN_REPODIR="-v /tmp/m2_docker:/root/.m2/repository";
-fi
+# just for resolving all dependencies
+mvn install -DskipTests
 
-args="--name=webdriver-test-$$ -m 1G --memory-swap=-1 -v /tmp/webdriver-tests-$USER:/tmp/webdrivers-root -v /dev/shm/:/dev/shm -v /tmp/webdrivers_docker:/root/tmp_webdrivers $MAVEN_REPODIR "
-
-rm -rf docker_target
-docker build -t test-$$ . | tee docker_build.log
-export IMAGE_ID=$(tail -n1 docker_build.log| cut -d\  -f3)
-export CID=$(docker run -d $args $IMAGE_ID bash -c 'while(true);do sleep 1;done')
-
-echo -n $IMAGE_ID > docker_IMAGE_ID
-echo -n $CID > docker_CID
-
-function cleanup {
-  if [ -n "$JENKINS_HOME" ]; then
-     echo "don't cleanup leave it for jenkins"
-     exit 0
-  fi
-  ./run_tests_cleanup.sh
-}
-trap cleanup EXIT INT
-
-docker exec $CID ./run_inside_docker.sh
-
-
+# actual running tests
+mvn clean
+timeout --preserve-status --kill-after 5m 4m mvn verify -Dxvfb.display=20
