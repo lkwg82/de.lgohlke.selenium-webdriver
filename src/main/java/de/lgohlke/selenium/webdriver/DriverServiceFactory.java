e6ac055ebd618d766eed4d25516a5f38fa8b0249
Lars K.W. Gohlke <lkwg82@gmx.de>
2015-10-23 15:16:39 +0200

    DriverServiceFactory no silently retries some time to connect to the webdriver (resilience)

--- src/main/java/de/lgohlke/selenium/webdriver/DriverServiceFactory.java
+++ src/main/java/de/lgohlke/selenium/webdriver/DriverServiceFactory.java
@@ -2,20 +2,45 @@ package de.lgohlke.selenium.webdriver;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.service.DriverService;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
 public abstract class DriverServiceFactory<S extends DriverService, T extends DriverConfiguration> {
     @Getter
     private final T driverConfiguration;
+    @Setter
+    private RemoteWebdriverInitialConnectionRetryer remoteWebdriverInitialConnectionRetryer = new RemoteWebdriverInitialConnectionRetryer(2, TimeUnit.SECONDS, 5);
 
     public abstract S createService(String... args);
 
     public WebDriver createWebDriver(S service) throws IOException {
+        DriverServiceFactory<S, T> outerFactory = this;
+
+        return remoteWebdriverInitialConnectionRetryer.start(new DriverServiceFactory<S, T>(driverConfiguration) {
+            @Override
+            public S createService(String... args) {
+                return outerFactory.createService(args);
+            }
+
+            @Override
+            public DriverArgumentsBuilder createServiceArgumentsBuilder() {
+                return outerFactory.createServiceArgumentsBuilder();
+            }
+
+            @Override
+            public WebDriver createWebDriver(S service) throws IOException {
+                return outerFactory.createPlainWebDriver(service);
+            }
+        }, service);
+    }
+
+    public WebDriver createPlainWebDriver(S service) throws IOException {
         return new RemoteWebDriver(service.getUrl(), getDriverConfiguration().createCapabilities());
     }
 
