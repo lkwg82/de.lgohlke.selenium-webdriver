8622fc445490b0dc06c6aca625b7188b4080c280
Lars K.W. Gohlke <lkwg82@gmx.de>
2015-10-23 23:10:26 +0200

    debug: docker test run

--- run_inside_docker.sh
+++ run_inside_docker.sh
@@ -3,8 +3,9 @@
 set -e
 
 # just for resolving all dependencies
-mvn install -DskipTests
+#mvn install -DskipTests
 
 # actual running tests
 mvn clean
-timeout --preserve-status --kill-after 5m 4m mvn verify -Dxvfb.display=20
+#timeout --preserve-status --kill-after 5m 4m mvn verify -Dxvfb.display=20
+mvn test
--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
 set -e
-
+set -x
 # $ export MAVEN_REPODIR="-v /tmp/m2_docker:/root/.m2/repository"; ./run_tests.sh
 
 if [ -z "$MAVEN_REPODIR" ]; then
     export MAVEN_REPODIR="-v /tmp/m2_docker:/root/.m2/repository";
 fi
 
-docker build -t test .
-docker run -m 1G $MAVEN_REPODIR -v /tmp/webdriver-tests-$USER:/tmp/webdrivers-root -v /dev/shm/:/dev/shm -t test ./run_inside_docker.sh
+args="--name=webdriver-test-$$ -m 1G --memory-swap=-1 -v /tmp/webdriver-tests-$USER:/tmp/webdrivers-root -v /dev/shm/:/dev/shm -v /tmp/webdrivers_docker:/root/tmp_webdrivers $MAVEN_REPODIR "
+
+rm -rf docker_target
+docker build -t test-$$ . | tee docker_build.log
+export IMAGE_ID=$(tail -n1 docker_build.log| cut -d\  -f3)
+export CID=$(docker run -d $args $IMAGE_ID bash -c 'while(true);do sleep 1;done')
+
+echo -n $IMAGE_ID > docker_IMAGE_ID
+echo -n $CID > docker_CID
+
+function cleanup {
+  docker cp $CID:/home/build/target docker_target
+
+  #docker kill $CID
+  docker rm  -f $CID
+  docker rmi  $IMAGE_ID
+}
+trap cleanup EXIT INT
+
+docker exec $CID ./run_inside_docker.sh
+
+
