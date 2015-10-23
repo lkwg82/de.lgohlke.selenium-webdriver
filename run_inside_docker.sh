c13b4c5de8c0fd5e1f5ae30c1d5e9020b1aa05e1
Lars K.W. Gohlke <lkwg82@gmx.de>
2015-10-23 23:25:53 +0200

    reactivated full docker test run

--- run_inside_docker.sh
+++ run_inside_docker.sh
@@ -3,9 +3,8 @@
 set -e
 
 # just for resolving all dependencies
-#mvn install -DskipTests
+mvn install -DskipTests
 
 # actual running tests
 mvn clean
-#timeout --preserve-status --kill-after 5m 4m mvn verify -Dxvfb.display=20
-mvn test
+timeout --preserve-status --kill-after 5m 4m mvn verify -Dxvfb.display=20
