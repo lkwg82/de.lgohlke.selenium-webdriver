7549db3fa098ee4debb767476acef400bce19369
Lars K.W. Gohlke <lkwg82@gmx.de>
2015-10-24 00:27:23 +0200

    fixed: docker copy path

--- run_docker_cleanup.sh
+++ run_docker_cleanup.sh
@@ -3,7 +3,8 @@
 CID=$(cat docker_CID)
 IMAGE_ID=$(cat docker_IMAGE_ID)
 
-docker cp $CID:/home/build/target target
+rm -rf target
+docker cp $CID:/home/build/target .
 
 docker kill  $CID
 docker rm -f $CID
