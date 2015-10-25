2e408fc147c69b15eb9ae67642f951ed37021065
Lars K.W. Gohlke <lkwg82@gmx.de>
2015-10-25 22:06:06 +0100

    reduced run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-set -e
-
 # just for resolving all dependencies
 mvn install -DskipTests
 
 # actual running tests
+timeout --preserve-status --kill-after 7m 6m mvn clean verify -P sonar-coverage
 
-mvn clean
-timeout --preserve-status --kill-after 7m 6m mvn verify -P sonar-coverage
+echo "exit code $?"
