0e927858b713cd8df204aad1dc58ee328b8de630
Lars K.W. Gohlke <lkwg82@gmx.de>
2015-10-26 08:48:11 +0100

    issues with docker makes the run_docker_cleanup.sh fail with exitcode 137

--- run_docker_cleanup.sh
+++ run_docker_cleanup.sh
@@ -9,19 +9,21 @@ docker cp $CONTAINER_ID:/home/build/installed_software.log target
 
 # check for issues (low memory etc.)
 if [ $(docker exec $CONTAINER_ID dmesg -T | grep docker-$CONTAINER_ID | wc -l) -gt 0 ]; then
-        echo "";
-        echo "[ERROR] there were issues with $CONTAINER_ID:";
-        echo "";
-        docker exec $CONTAINER_ID dmesg -T | grep docker-$CONTAINER_ID
-        echo "--"
-        echo "host config looks like (reduced)"
-        docker inspect --format="{{json .HostConfig}}" $CONTAINER_ID | python -m json.tool \
-            | grep -v ": \"\"" \
-            | grep -v ": null" \
-            | grep -vE ": 0|-1" \
-            | grep -v ": \[\]" \
-            | grep -v ": {}"
-    fi
+    echo "";
+    echo "[ERROR] there were issues with $CONTAINER_ID:";
+    echo "";
+    docker exec $CONTAINER_ID dmesg -T | grep docker-$CONTAINER_ID
+    echo "--"
+    echo "host config looks like (reduced)"
+    docker inspect --format="{{json .HostConfig}}" $CONTAINER_ID | python -m json.tool \
+        | grep -v ": \"\"" \
+        | grep -v ": null" \
+        | grep -vE ": 0|-1" \
+        | grep -v ": \[\]" \
+        | grep -v ": {}"
+
+    exit 137
+fi
 
 # cleanup instances
 docker kill  $CONTAINER_ID
