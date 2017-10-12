407bc3f9ddb40ce703dc7a6589b847be03ab49e1
Lars K.W. Gohlke <lkwg82@gmx.de>
2017-10-12 14:44:37 +0200

    removes unues script

--- add_to_latest_tag_changelog.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git log --oneline $(git tag | tail -n2 |xargs | sed -e 's# #...#') | grep -v "\[maven-release-plugin\]" > target/changelog.txt
-
-latestTag=$(git tag | tail -n1)
-git tag -a $latestTag -f -F target/changelog.txt
-
-git push --force origin refs/tags/$latestTag
