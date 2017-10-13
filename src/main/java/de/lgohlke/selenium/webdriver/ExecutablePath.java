ec421c9f7e8479f6be6da5b4e7fee2442bab680c
Lars K.W. Gohlke <lkwg82@gmx.de>
2017-10-13 10:35:00 +0200

    cleanup test

--- src/main/java/de/lgohlke/selenium/webdriver/ExecutablePath.java
+++ src/main/java/de/lgohlke/selenium/webdriver/ExecutablePath.java
@@ -1,14 +1,12 @@
 package de.lgohlke.selenium.webdriver;
 
-import com.google.common.annotations.VisibleForTesting;
-
 import java.io.File;
 import java.nio.file.Paths;
 
 public class ExecutablePath {
     public File buildExecutablePath(String basename) {
         String environmentVariable = "DRIVERS_PATH";
-        String driversPath         = getEnvironment(environmentVariable);
+        String driversPath         = System.getenv(environmentVariable);
 
         if (driversPath == null) {
             throw new IllegalStateException("please set environment variable " + environmentVariable + " to point to the directory where " + basename + " binary is located");
@@ -16,17 +14,12 @@ public class ExecutablePath {
 
         boolean is64bit;
         if (System.getProperty("os.name").contains("Windows")) {
-            is64bit = getEnvironment("ProgramFiles(x86)") != null;
+            is64bit = System.getenv("ProgramFiles(x86)") != null;
         } else {
             is64bit = System.getProperty("os.arch").contains("64");
         }
         String filename = basename + "-" + System.getProperty("os.name")
                                                  .toLowerCase() + "-" + (is64bit ? "64" : "32") + "bit";
         return Paths.get(driversPath, filename).toFile();
-    }
-
-    @VisibleForTesting
-    String getEnvironment(String environmentVariable) {
-        return System.getenv(environmentVariable);
     }
 }
--- src/test/java/de/lgohlke/selenium/webdriver/ExecutablePathTest.java
+++ src/test/java/de/lgohlke/selenium/webdriver/ExecutablePathTest.java
@@ -1,33 +1,25 @@
 package de.lgohlke.selenium.webdriver;
 
-import com.google.common.collect.ImmutableMap;
-import lombok.RequiredArgsConstructor;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
 
 import java.io.File;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ExecutablePathTest {
+    @Rule
+    public final  EnvironmentVariables environmentVariables = new EnvironmentVariables();
+    private final ExecutablePath       executablePath       = new ExecutablePath();
+
     @Test
     public void test() {
-        ImmutableMap<String, String> environment = ImmutableMap.of("DRIVERS_PATH", "/tmp");
-        ExecutablePathMock           executablePath = new ExecutablePathMock(environment);
+        environmentVariables.set("DRIVERS_PATH", "/tmp");
 
-        System.setProperty("os.name","Linux");
-        System.setProperty("os.arch","x64");
+        System.setProperty("os.name", "Linux");
+        System.setProperty("os.arch", "x64");
 
         assertThat(executablePath.buildExecutablePath("p")).isEqualTo(new File("/tmp/p-linux-64bit"));
-    }
-
-    @RequiredArgsConstructor
-    private static class ExecutablePathMock extends ExecutablePath {
-        private final Map<String, String> environment;
-
-        @Override
-        String getEnvironment(String environmentVariable) {
-            return environment.get(environmentVariable);
-        }
     }
 }
